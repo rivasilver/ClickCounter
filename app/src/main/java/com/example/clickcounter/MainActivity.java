2eea362d3d579287d044abd731d20844a35db656
Vágó Rebeka <rebeka.vago19@gmail.com>
2022-10-07 02:08:57 +0200

    main activity inicializálás kész

--- app/src/main/java/com/example/clickcounter/MainActivity.java
+++ app/src/main/java/com/example/clickcounter/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.clickcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private RelativeLayout relativeLayout;
+    private TextView textViewNumber;
+    private Button buttonPlus;
+    private Button buttonMinus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    public void init(){
+        relativeLayout = findViewById(R.id.relativeLayout);
+        textViewNumber = findViewById(R.id.textViewNumber);
+        buttonPlus = findViewById(R.id.buttonPlus);
+        buttonMinus = findViewById(R.id.buttonMinus);
     }
 }
